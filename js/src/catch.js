ea9ac0099903bb8c5b158894e1dc4d0169106955
kngines <kngines@163.com>
2019-04-14 02:52:17 +0800

    Site updated: 2019-04-14 02:52:16

---
+++ js/src/catch.js
@@ -0,0 +1,3 @@
+// build time:Sun Apr 14 2019 02:50:42 GMT+0800 (CST)
+var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="∑(っ°Д°;)っah, 不见了 ~ ";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ᕙ༼ ͝°益° ༽ᕗ 又好了 ~ "+OriginTitile;titleTime=setTimeout(function(){document.title=OriginTitile},2e3)}});
+//rebuild by neat 
