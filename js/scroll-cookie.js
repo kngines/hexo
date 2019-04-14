160eb61cb2e269d34c106806b5c1f3564b404290
kngines <kngines@163.com>
2019-04-15 00:32:11 +0800

    Site updated: 2019-04-15 00:32:10

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 14 2019 02:50:42 GMT+0800 (CST)
+// build time:Sun Apr 14 2019 22:22:36 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
